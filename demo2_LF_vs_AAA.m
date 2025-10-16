bcd24c850ce97a72c5845be6b8b3f837f2c73c00
Charles Poussot-Vassal <charles.poussot@gmail.com>
2025-10-16 16:38:40 +0200

    demo color

--- demo2_LF_vs_AAA.m
+++ demo2_LF_vs_AAA.m
@@ -109,7 +109,7 @@ for ii = 1:numel(AAAparam)
     axis equal, set(gca,'Xlim',data.Xlim,'YLim',data.Ylim)
     ylabel('Imag(.)'), xlabel('Real(.)')
     title({['\textbf{AAA} \texttt{opt=' AAAparam_i{1}(2:end) '}']; ...
-           ['$r=' num2str(robj) '$ (' num2str(length(h3poles)) '), $\sigma_r=$ ' num2str(abs(rsig)) ' in ' num2str(timeAAA) 's' ]})
+           ['$r=' num2str(robj) '$ ($' num2str(length(h3poles)) '$), $\sigma_r=$ ' num2str(abs(rsig)) ' in ' num2str(timeAAA) 's' ]})
     drawnow
 end
 license('inuse')
